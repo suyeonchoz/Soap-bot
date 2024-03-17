d3cebaf4da43c58a1be2057d6ac746f4562196f2
sanujz <156044793+sanujz@users.noreply.github.com>
2024-03-17 11:35:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  OWNER_IDS: ["885368385595125791"], // Bot owner ID's
+  OWNER_IDS: ["885368385595125791" , "869229454927462400" ], // Bot owner ID's
   SUPPORT_SERVER: "https://discord.gg/7ttXPmZUz9", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
